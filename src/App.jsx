d60ed12c17ce08ca33f7019cd3a1f8dac4fbce45
Luis Felipe Salvarani <felipe_salvarani@hotmail.com>
2024-06-14 17:39:26 -0300

    Finalização da página inicial

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import StartScreen from './components/StartScreen'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <h2>Secret Word</h2>
+      <StartScreen />
     </>
   )
 }
